a9fc6854f882f4cb909cb67b01ff4f2b995a0e4c
Kevin Glover-Netherton <kevin.glovernetherton@gmail.com>
2018-11-13 13:01:28 -0500

    Moved hero to separate component

--- src/components/LoginHome.vue
+++ src/components/LoginHome.vue
@@ -1,9 +1,6 @@
 <template>
     <div id="login-container">
-      <div class="hero">
-        <img src="@/assets/images/logo.png" alt="Logo">
-        <h1>Beacon</h1>
-      </div>
+      <LoginHomeHero />
       <div class="form">
         <LoginForm />
       </div>
@@ -12,11 +9,13 @@
 
 <script>
 import LoginForm from '@/components/LoginForm.vue'
+import LoginHomeHero from '@/components/LoginHomeHero.vue'
 
 export default {
   name: 'LoginHome',
   components: {
-    LoginForm
+    LoginForm,
+    LoginHomeHero
   }
 }
 </script>
@@ -32,25 +31,5 @@ export default {
 .form {
   padding: 0em 2em;
   width: 60%;
-}
-
-.hero {
-  background: $primary;
-  min-height: 60%;
-  padding: 6em 0em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  width: 40%;
-
-  img {
-    width: 100px;
-  }
-
-  h1 {
-    color: $dark;
-    font-weight: normal;
-  }
 }
 </style>
