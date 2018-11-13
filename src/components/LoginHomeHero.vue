a4f42ee44da1d70a6c95721f8ad4175fc1b46040
Kevin Glover-Netherton <kevin.glovernetherton@gmail.com>
2018-11-13 17:15:00 -0500

    Added animation on failed login attempt

---
+++ src/components/LoginHomeHero.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hero">
+    <img src="@/assets/images/logo.png" alt="Logo">
+    <h1>Beacon</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginHomeHero'
+}
+</script>
+
+<style lang="scss">
+
+.hero {
+  background: $primary;
+  min-height: 60%;
+  padding: 6em 0em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 40%;
+
+  img {
+    width: 100px;
+  }
+
+  h1 {
+    color: $dark;
+    font-weight: normal;
+  }
+}
+</style>
