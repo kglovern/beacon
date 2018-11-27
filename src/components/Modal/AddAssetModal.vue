63628e95470b245ccf51e5a03352af56b778cb38
Ethan Lewis <elewis06@uoguelph.ca>
2018-11-27 11:33:02 -0500

    Fleshing out add asset form.

--- src/components/Modal/AddAssetModal.vue
+++ src/components/Modal/AddAssetModal.vue
@@ -28,7 +28,7 @@
           </div>
         </form>
         <div id="asset-action">
-          <button class="button" id="cancel-asset">Cancel</button>
+          <button @click="d" class="button" id="cancel-asset">Cancel</button>
           <button class="button" id="submit-asset">Submit</button>
         </div>
       </div>
