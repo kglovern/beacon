a7456124881ef1aac2b33e8f89fd466dbe8bc58c
Kevin Glover-Netherton <kglovern@mail.uoguelph.ca>
2018-11-27 06:55:02 -0500

    calling backend create project

--- src/router.js
+++ src/router.js
@@ -5,37 +5,46 @@ import LoginPage from './views/LoginPage.vue'
 import ManagementSystem from './views/ManagementSystem.vue'
 import Client from './views/Client.vue'
 import Library from '@/views/Library.vue'
+import CreateProject from '@/components/ProjectListing/CreateProject.vue'
+import ProjectListings from '@/components/ProjectListing/ProjectListings.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/projects',
-      name: 'projects',
-      component: Projects
+  routes: [{
+    path: '/projects',
+    name: 'projects',
+    component: Projects,
+    children: [{
+      path: 'create',
+      component: CreateProject
     },
     {
       path: '/',
-      name: 'login',
-      component: LoginPage
-    },
-    {
-      path: '/manage',
-      name: 'manage',
-      component: ManagementSystem
-    },
-    {
-      path: '/client',
-      name: 'client',
-      component: Client
-    },
-    {
-      path: '/library',
-      name: 'library',
-      component: Library
-    }
+      component: ProjectListings
+    }]
+  },
+  {
+    path: '/',
+    name: 'login',
+    component: LoginPage
+  },
+  {
+    path: '/manage',
+    name: 'manage',
+    component: ManagementSystem
+  },
+  {
+    path: '/client',
+    name: 'client',
+    component: Client
+  },
+  {
+    path: '/library',
+    name: 'library',
+    component: Library
+  }
   ]
 })
